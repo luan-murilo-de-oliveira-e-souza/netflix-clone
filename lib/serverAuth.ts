f64c602b1a0f9265ea14783738ea590cd950eff7
Luan Souza <107760950+luan-murilo-de-oliveira-e-souza@users.noreply.github.com>
2023-06-03 17:48:35 +0100

    protect client routes

---
+++ lib/serverAuth.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest } from "next";
+import { getSession} from 'next-auth/react';
+
+import prismadb from '@/lib/prismadb';
+
+const serverAuth = async (req: NextApiRequest) => {
+        
+    const session = await getSession ({req}) //object that accept our request
+
+    //check if the session, user, email exist
+    if (!session?.user?.email){
+        throw new Error('Not signed in');
+    }
+
+    const currentUser = await prismadb.user.findUnique({
+        where: {
+            email: session.user.email,
+        }
+    });
+
+    if (!currentUser) {
+        throw new Error('Not signed in');
+    }
+
+    return {currentUser};
+}
+
+export default serverAuth;
