c9c25f3468edfab510d15d4bd1d4a45bb74d71e7
Luan Souza <107760950+luan-murilo-de-oliveira-e-souza@users.noreply.github.com>
2023-06-04 09:41:29 +0100

    create user hook

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,38 @@
+import userCurrentUser from '@/hooks/useCurrentUser';
+import { NextPageContext } from 'next'
+import {getSession, signOut} from 'next-auth/react'
+
+//protect home route
+export async function getServerSideProps(context: NextPageContext){
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+
+}
+
+  
 export default function Home() {
+  //fetch our user using our user hook
+  //create alias for data
+  const {data: user} = userCurrentUser();
+
   return (
     <>
-     <h1 className="text-2xl text-green-500">Netflix Clone</h1>
+     <h1 className="text-4xl text-green-500">Netflix Clone</h1>
+     {/*show your email when you logged in */}
+     <p className='text-white'>Logged in as: {user?.email}</p>
+     <button className='h10 w-full bg-white' onClick={() => signOut()}>Logout!</button>
     </>
   )
 }
