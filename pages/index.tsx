2cedc85a5d95202b34a4af5f1a8392ed39661f0d
Luan Souza <107760950+luan-murilo-de-oliveira-e-souza@users.noreply.github.com>
2023-06-05 18:13:42 +0100

    create navbaritem

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
-import userCurrentUser from '@/hooks/useCurrentUser';
 import { NextPageContext } from 'next'
 import {getSession, signOut} from 'next-auth/react'
+
+import Navbar from '@/components/navbar';
 
 //protect home route
 export async function getServerSideProps(context: NextPageContext){
@@ -23,16 +24,10 @@ export async function getServerSideProps(context: NextPageContext){
 
   
 export default function Home() {
-  //fetch our user using our user hook
-  //create alias for data
-  const {data: user} = userCurrentUser();
 
   return (
     <>
-     <h1 className="text-4xl text-green-500">Netflix Clone</h1>
-     {/*show your email when you logged in */}
-     <p className='text-white'>Logged in as: {user?.email}</p>
-     <button className='h10 w-full bg-white' onClick={() => signOut()}>Logout!</button>
+      <Navbar />
     </>
   )
 }
