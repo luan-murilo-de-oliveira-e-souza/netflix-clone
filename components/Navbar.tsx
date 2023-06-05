fce22e840f75ddd9e5c5fbb5c1a8514d56448b7e
Luan Souza <107760950+luan-murilo-de-oliveira-e-souza@users.noreply.github.com>
2023-06-05 21:07:10 +0100

    create account menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,15 +3,24 @@ import {BsChevronDown, BsSearch, BsBell} from 'react-icons/bs';
 
 import NavbarItem from "./NavbarItem";
 import MobileMenu from "./MobileMenu";
+import AccountMenu from "./AccountMenu";
 
 const Navbar = () => {
     //used for action on browse
     const [showMobileMenu, setShowMobileMenu] = useState (false);
+    const [showAccountMenu, setShowAccountMenu] = useState (false);
+
 
     const toggleMobileMenu = useCallback(() => {
         // reverse current
         setShowMobileMenu((current) => !current);
-      }, []);
+    }, []);
+
+    const toggleAccountMenu = useCallback(() => {
+        // reverse current
+        setShowAccountMenu((current) => !current);
+    }, []);
+
 
     return (
         <nav className="w-full fixed z-40">
@@ -49,7 +58,7 @@ const Navbar = () => {
 
                 <div onClick={toggleMobileMenu} className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative">
                     <p className="text-white text-sm">Browse</p>
-                    <BsChevronDown className="text-white transition"/>
+                    <BsChevronDown className={`text-white transition ${showMobileMenu? 'rotate-180' : 'rotate-0'}`}/>
                     <MobileMenu visible={showMobileMenu}/>
                 </div>
                 <div className="flex flex-row ml-auto gap-7 items-center">
@@ -59,11 +68,13 @@ const Navbar = () => {
                     <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition">
                         <BsBell />
                     </div>
-                    <div className="flex flex-row items-center gap-2 cursor-pointer relative">
+                    <div onClick={toggleAccountMenu} className="flex flex-row items-center gap-2 cursor-pointer relative">
                         <div className="w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden">
                             <img src="/images/default-blue.png" alt="" />
                         </div>
-                        <BsChevronDown className="text-white transition"/>
+                        {/* Rotate chevron when I click */}
+                        <BsChevronDown className={`text-white transition ${showAccountMenu? 'rotate-180' : 'rotate-0'}`}/>
+                        <AccountMenu visible={showAccountMenu}/>
                     </div>
                 </div>
             </div>
